3584aedfa5eacb9b4624a8e142c406785afaa40a
Taiye Salami <taiye41@gmail.com>
2020-04-06 14:33:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -50,7 +50,7 @@ def game_hash
           :slam_dunks => 5
         },
         {
-          :player_name => "Jason Terry",
+          :player_name => "Jasn Terry",
           :number => 31,
           :shoe => 15,
           :points => 19,
